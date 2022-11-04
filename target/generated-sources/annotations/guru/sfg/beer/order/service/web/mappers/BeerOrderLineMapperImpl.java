3caedaef39caea7ff2678087b796f1268598e457
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-04 15:43:01 +0100

    trying to fix problem with different version with course

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-03T18:42:33+0100",
+    date = "2022-11-04T15:32:24+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
