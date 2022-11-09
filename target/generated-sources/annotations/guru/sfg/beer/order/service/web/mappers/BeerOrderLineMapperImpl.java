07cb029626349abb2bd4da318b3ee937d4954a33
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-09 13:44:09 +0100

    setup allocation state machine states

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-09T13:37:18+0100",
+    date = "2022-11-09T13:39:28+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
