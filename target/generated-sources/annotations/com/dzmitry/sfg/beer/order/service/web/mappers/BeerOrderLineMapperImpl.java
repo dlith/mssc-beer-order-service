92a6215fe410028a11e630d3f00562bf6935ff09
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-20 12:49:50 +0100

    adding and configure spring cloud sleuth

--- target/generated-sources/annotations/com/dzmitry/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/com/dzmitry/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-19T15:12:13+0100",
+    date = "2022-11-20T12:37:46+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
